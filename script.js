4dd1ff5af00856b13e3c6077a4920246aff0c9c2
Clayton <claytonphilippe@gmail.com>
2021-08-17 21:41:31 -0300

    criar background

--- script.js
+++ script.js
@@ -3,9 +3,9 @@ let context = canvas.getContext("2d");
 let box = 32; //32 pixels cada quadrado
 
 function criarBG(){
-  context.fillStyle = "lightgreen";
+  context.fillStyle = '#9BC404';
   context.fillRect(0, 0, 16 * box, 16 * box); /*Estilo do canvas, fillreact
   vai desenhar o nosso retangulo */
 }
 
-
+criarBG();
