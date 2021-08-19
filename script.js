fdd0a4f5112de3edee25e5f2dca31ba53af3e686
Clayton <claytonphilippe@gmail.com>
2021-08-19 20:01:23 -0300

    definindo colisão/game over

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 let canvas = document.getElementById("snake");
 let context = canvas.getContext("2d");
-let box = 32; //32 pixels cada quadrado
-let snake = []; //array
-snake[0] = { //o que vai ter dentro da cobrinha
-  x: 8 * box, //posição de onde irá começar a cobrinha
+let box = 32; 
+let snake = []; 
+snake[0] = { 
+  x: 8 * box, 
   y: 8 * box,
 }
 
@@ -15,15 +15,13 @@ let food = {
 
 function background() {
   context.fillStyle = "#9BC404";
-  context.fillRect(0, 0, 16 * box, 16 * box); /*Estilo do canvas, fillreact
-  vai desenhar o nosso retângulo, área do canvas */
+  context.fillRect(0, 0, 16 * box, 16 * box);
 }
 
 function criarCobrinha() {
-  for (i = 0; i < snake.length; i++) { /*vai percorrer todo tamanho do array,
-    vai pintando a cobrinha*/
-    context.fillStyle = "#000203"; //cor da cobrinha
-    context.fillRect(snake[i].x, snake[i].y, box, box); //tamanho da cobrinha
+  for (i = 0; i < snake.length; i++) { 
+    context.fillStyle = "#000203";
+    context.fillRect(snake[i].x, snake[i].y, box, box); 
   }
 }
 
@@ -47,15 +45,21 @@ function iniciarJogo(){
   criarCobrinha();
   drawFood();
 
-  let snakeX = snake[0].x;//ponto de partida
+  let snakeX = snake[0].x;
   let snakeY = snake[0].y;
 
-  if(direction == "right") snakeX += box; //vai acrescentando quadrado da cobrinha
-  if(direction == "left") snakeX -= box; //
-  if(direction == "up") snakeY -= box; //
-  if(direction == "down") snakeY += box; //
+  if(direction == "right") snakeX += box; 
+  if(direction == "left") snakeX -= box; 
+  if(direction == "up") snakeY -= box; 
+  if(direction == "down") snakeY += box; 
 
-  snake.pop(); //retira o último elemento do array
+if(snakeX != food.x || snakeY != food.y){
+  snake.pop();
+}
+else{
+  food.x = Math.floor(Math.random() * 15 + 1) * box, 
+  food.y = Math.floor(Math.random() * 15 + 1) * box
+}
 
   let newHead = {
     x: snakeX,
@@ -68,5 +72,4 @@ function iniciarJogo(){
 }
 
 
-let jogo = setInterval(iniciarJogo, 300); /*intervalo se 100 milissegundos
-para iniciar jogo*/
+let jogo = setInterval(iniciarJogo, 100); 
