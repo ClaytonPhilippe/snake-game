9073088876d5063ce790989bac867495d87c7184
Clayton <claytonphilippe@gmail.com>
2021-08-19 20:09:53 -0300

    definindo estilos

--- script.js
+++ script.js
@@ -41,6 +41,15 @@ function update (event){
 }
 
 function iniciarJogo(){
+
+ for(i = 1; i < snake.length; i++){ //colisão
+   if(snake[0].x == snake[i].x && snake[0].y == snake[i].y){
+     clearInterval(jogo);
+     alert('Game Over')
+   }
+ }
+
+
   background();
   criarCobrinha();
   drawFood();
